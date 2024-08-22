67679f880b696b46a32f198993f30aa5a4d11111
Serhan Çakmak <cakmakserhan02@gmail.com>
2024-08-22 16:43:24 +0300

    add login

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path("player", views.player, name="player"),
+    path("admin", views.admin, name="admin"),
+    path("coach", views.coach, name="match"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from dotenv import load_dotenv
+import os
+import mysql.connector
 
-# Create your views here.
+from .utils.forms import *
+
+load_dotenv()
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password=os.environ.get("password"),
+    database="volleydb",
+    buffered=True
+)
+
+
 def home(request):
-    context = {"message": "Hello, World!"}
+    print(request)
+    if request.method == "POST":
+        form = user_form(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            mycursor = mydb.cursor()
+            mycursor.execute("select * from user u where u.username=%s and u.password=%s", (username, password)),
+            myresult = mycursor.fetchone()
+            mycursor.close()
+            if myresult:
+                mycursor = mydb.cursor()
+                mycursor.execute("select * from player u where u.username=%s", (username,))
+                myresult = mycursor.fetchone()
+                mycursor.close()
+
+                if myresult:
+                    print("here")
+                    return redirect("player")
+
+                mycursor = mydb.cursor()
+                mycursor.execute("select * from coach u where u.username=%s", (username,))
+                myresult = mycursor.fetchone()
+                mycursor.close()
+                if myresult:
+                    return redirect("coach")
+
+                return redirect("admin")
+
+            else:
+                form = user_form()
+                context = {"form": form,
+                           "message": "Invalid username or password"}
+
+    else:
+        print("here4")
+        form = user_form()
+        context = {"form": form}
+
     return render(request, 'home.html', context)
+
+
+def player(request):
+    return render(request, 'player.html')
+
+
+def coach(request):
+    return render(request, 'coach.html')
+
+
+def admin(request):
+    return render(request, 'admin.html')
